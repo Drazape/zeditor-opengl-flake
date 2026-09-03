9f8a13ab54bf2062e2fd51151ce42e732b9e7341
Drazape <Drazape@outlook.com>
2026-09-03 19:22:44 +0530

    feat(source): official → Nixpkgs
    The latest official flake is broken. Use the stable zed-editor-fhs version.
    This also guarantees that GitHub copilot would work with it.

--- flake.nix
+++ flake.nix
@@ -4,32 +4,23 @@
   inputs = {
 		flake-parts = { type="github"; owner="hercules-ci"; repo="flake-parts"; };
 		nixpkgs = { type="github"; owner="NixOS"; repo="nixpkgs"; ref="nixpkgs-unstable"; };
-		zeditor = {
-			type="github"; owner="zed-industries"; repo="zed";
-			inputs = {
-				nixpkgs.follows = "nixpkgs";
-				flake-parts.follows = "flake-parts";
-			};
-		};
   };
 
 	outputs = inputs@{ flake-parts, ... }:
 		flake-parts.lib.mkFlake { inherit inputs; } {
 			systems = ["x86_64-linux"];
-			perSystem = { inputs', ... }: {
+			perSystem = { pkgs, ... }: {
 				packages = {
-				  zeditor-opengl-src = inputs'.zeditor.packages.default.override { withGLES = true; };
-				};
+				  zeditor-opengl-src = pkgs.zed-editor.overrideAttrs (finalAttrs: previousAttrs: {
+            # inject the GLES configuration flag required by Zed's gpui crate
+            env = (previousAttrs.env or { }) // {
+              RUSTFLAGS = "--cfg gles";
+            };
+            
+            # explicitly ensure libGL is available during the CI build
+            buildInputs = (previousAttrs.buildInputs or [ ]) ++ [ pkgs.libGL ];
+          });
+        };
 			};
 		};
-
-	# from official Zed flake
-	nixConfig = {
-		extra-substituters = [
-			"https://zed.cachix.org"
-		];
-		extra-trusted-public-keys = [
-			"zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
-		];
-	};
 }
