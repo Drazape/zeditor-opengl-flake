935d19f691648148d87a34bae000a7c10c283504
Drazape <Drazape@outlook.com>
2026-09-03 17:28:19 +0530

    feat(flake): Pre-compiled package
    Use the release artifacts to re-prepare the final package for use in our NixOS systems

--- flake.nix
+++ flake.nix
@@ -4,13 +4,33 @@
   inputs = {
 		flake-parts = { type="github"; owner="hercules-ci"; repo="flake-parts"; };
 		nixpkgs = { type="github"; owner="NixOS"; repo="nixpkgs"; ref="nixpkgs-unstable"; };
+		zeditor-opengl-release = {
+			url = "https://github.com/Drazape/zeditor-opengl-flake/releases/latest/download/zed-opengl-x86_64-linux.tar.gz";
+			flake = false;
+		};
   };
 
 	outputs = inputs@{ flake-parts, ... }:
 		flake-parts.lib.mkFlake { inherit inputs; } {
 			systems = ["x86_64-linux"];
-			perSystem = { pkgs, ... }: {
+			perSystem = { self', pkgs, lib, ... }: {
 				packages = {
+					default = self'.packages.zeditor-opengl;
+					zeditor-opengl = pkgs.stdenvNoCC.mkDerivation {
+						name = "zeditor-opengl";
+						src = inputs.zeditor-opengl-release;
+
+						installPhase = ''
+							${lib.getExe' pkgs.coreutils "cp"} --recursive -- $src/ $out/
+						'';
+
+						meta = {
+							description = "Zeditor compiled for OpenGL graphics";
+							homepage = "https://github.com/Drazape/zeditor-opengl-flake";
+							license = lib.licenses.gpl3;
+							platforms = lib.platforms.linux;
+						};
+					};
 				  zeditor-opengl-src = pkgs.zed-editor.overrideAttrs (finalAttrs: previousAttrs: {
             # inject the GLES configuration flag required by Zed's gpui crate
             env = (previousAttrs.env or { }) // {
